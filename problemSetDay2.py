f43d7ff542b1acedd6b013da1f005e0976328b0a
ashfiaislam <ashfia.zara@gmail.com>
2019-02-16 17:29:28 +0000

    done

--- problemSetDay2.py
+++ problemSetDay2.py
@@ -1,1 +1,19 @@
 # file name: problemSetDay2.py
+name= input("what is your name?")
+name= "my name istomato"
+print(name)
+birthday= input("when is your birthday?")
+birthday= "my birthday is march 3rd"
+print(birthday)
+grade= input("what grade are you in?")
+grade= "I am in the 9th grade"
+print (grade)
+school= input("what school do you go to?")
+school= "I go to Curtis"
+print (school)
+favsubject= input("what is your fav subject?")
+favsubject= "my fav subject is bio."
+print (favsubject)
+growup= input("what do you want to do when you grow up?")
+growup= "i want to be a teacher"
+print (growup)
